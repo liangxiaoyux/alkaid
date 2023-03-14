c73f82299397b6a67195e90b463ee8c2a19b282f
潇宇同学 <pgjackliang@aliyun.com>
2023-03-15 00:08:58 +0800

    1.3

--- tiankui-core/src/main/java/com/kuaijie/tiankui/bean/entity/oa/production/ManufactureProjectState.java
+++ tiankui-core/src/main/java/com/kuaijie/tiankui/bean/entity/oa/production/ManufactureProjectState.java
@@ -6,16 +6,14 @@ import org.hibernate.annotations.Table;
 
 import javax.persistence.Entity;
 
-
 /**
  * @version 1.0
  * @auther 梁潇宇
- * @date 2023/3/14 23:29
- * @description 生产项目
+ * @date 2023/3/15 0:05
+ * @description
  */
-@Entity(name = "t_manufacture_prj")
-@Table(appliesTo = "t_manufacture_prj", comment = "生产项目")
+@Entity(name = "t_manufacture_prj_state")
+@Table(appliesTo = "t_manufacture_prj_state",comment = "生产项目状态")
 @Data
-public class ManufacturePrj extends BaseEntity {
-
+public class ManufactureProjectState extends BaseEntity {
 }
